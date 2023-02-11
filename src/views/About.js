85bd98f744c473b86ba4e3d5b027ca9a8830de28
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-02-11 20:37:27 +0530

    About Section Completed

--- src/views/About.js
+++ src/views/About.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../constant";
 
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+	const [ref, inView] = useInView({
+		threshold: 0.5,
+	});
+	return (
+		<section className="section" id="about" ref={ref}>
+			<div className="container mx-auto">
+				<div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+					{/* Image */}
+					<motion.div
+          variants={fadeIn('right',0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{once:false, amount:0.3}}
+          className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></motion.div>
+					{/* Content */}
+					<motion.div
+          variants={fadeIn('left',0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{once:false, amount:0.3}}
+          className="flex-1">
+						<h2 className="h2 text-accent">About Me.</h2>
+						<h3 className="h3 text-white">
+            Driving my passion for design, technology, and automobiles forward.
+						</h3>
+						<p className="mb-6">
+							A tech-savvy professional pursuing a Diploma in Computer
+							Engineering. With 2+ years of experience in graphic design and 1+
+							year in web development, I am a one-stop solution for all your
+							design and tech needs. My passion for technology and leadership
+							skills, honed me as the President of YoursthatSenior Club, making
+							me an exceptional problem-solver. Get Ready to bring your vision
+							to life? Let's work together !
+						</p>
+
+						{/* Stats */}
+						<div className="flex gap-x-6 lg:gap-x-10 mb-12">
+							<div>
+								<div className="text-[40px] font-teritary text-gradient mb-2">
+									{inView ? <CountUp start={0} end={13} duration={3} /> : null}
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Years of <br /> Experience
+								</div>
+							</div>
+              <div>
+								<div className="text-[40px] font-teritary text-gradient mb-2">
+									{inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                  k+
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Projects <br /> Completed
+								</div>
+							</div>
+              <div>
+								<div className="text-[40px] font-teritary text-gradient mb-2">
+									{inView ? <CountUp start={0} end={10} duration={3} /> : null}
+                  k+
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Satisfied <br/> Clients
+								</div>
+							</div>
+						</div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg"> Hire Me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+					</motion.div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
