f952d10f58c53dcf512430c1d43d96231cfc0a07
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-02-11 21:13:48 +0530

    90% of service component is finished

--- src/views/Services.js
+++ src/views/Services.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React from "react";
+import { BsArrowUpRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "../constant";
+
+const services = [
+	{
+		name: "UI/UX Design",
+		description:
+			"UI/UX Design creates intuitive and user-friendly digital experiences by combining design elements with functional purpose. It involves understanding user needs, designing interfaces, conducting testing and refining the final product. The goal is to provide seamless and aesthetically pleasing experiences for end users.",
+		link: "Learn More",
+	},
+	{
+		name: "Logo Design",
+		description:
+			"UI/UX Design creates intuitive and user-friendly digital experiences by combining design elements with functional purpose. It involves understanding user needs, designing interfaces, conducting testing and refining the final product. The goal is to provide seamless and aesthetically pleasing experiences for end users.",
+		link: "Learn More",
+	},
+	{
+		name: "Branding",
+		description:
+			"UI/UX Design creates intuitive and user-friendly digital experiences by combining design elements with functional purpose. It involves understanding user needs, designing interfaces, conducting testing and refining the final product. The goal is to provide seamless and aesthetically pleasing experiences for end users.",
+		link: "Learn More",
+	},
+	{
+		name: "Graphic Designing",
+		description:
+			"UI/UX Design creates intuitive and user-friendly digital experiences by combining design elements with functional purpose. It involves understanding user needs, designing interfaces, conducting testing and refining the final product. The goal is to provide seamless and aesthetically pleasing experiences for end users.",
+		link: "Learn More",
+	},
+];
 
 const Services = () => {
-  return <div className='section' id='services'>Services</div>;
+	return (
+		<section className="section" id="services">
+			<div className="container mx-auto">
+				<div className="flex flex-col lg:flex-row">
+					{/* text */}
+					<div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+						<h2 className="h2 text-accent mb-6"> What I Do</h2>
+						<h3 className="h3 max-w-[455px] mb-16">
+							Driving my passion for design, technology, and automobiles
+							forward.
+						</h3>
+						<button className="btn btn-sm"> Take a look at my work </button>
+					</div>
+					{/* services */}
+					<div className="flex-1">
+						{/* service list */}
+						<div>
+							{services.map((service, index) => {
+                //Destructuring Service
+                const {name, description, link} = service;
+								return (
+									<div className="border-b border-white/20 h-[190px] mb-[20px] flex" key={{index}}>
+										<div className="max-w-[476px]">
+											<h4 className="text-[20px] tracking-wider font-primary font-semibold mb-4">{name}</h4>
+                      <p className="font-secondary mb-4 leading-tight">{description}</p>
+										</div>
+										<div>
+                      <a href="#">
+                        <BsArrowUpRight/>
+                      </a>
+                      <a href="#">{link}</a>
+                    </div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Services;
