77ff98ca6d37d9cf1c5e3662f6afb65f114fbf70
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-02-15 22:50:10 +0530

    Update Work.js

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {motion} from 'framer-motion';
+import { fadeIn } from '../constant';
+import Img1 from '../assets/portfolio-img1.png';
+import Img2 from '../assets/portfolio-img2.png';
+import Img3 from '../assets/portfolio-img3.png';
 
 const Work = () => {
   return <div className='section' id='work'>Work</div>;
